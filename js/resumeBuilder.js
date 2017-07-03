5715d2b9c8d095638fb0c8de36e2bc79ffc09ca6
DESKTOP-Q6D10N1\dsison <dsison34@hotmail.com>
2017-07-02 19:10:47 -0700

    Finished Lesson 3.7

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,21 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// $("#main").append("Dustin Sison");
+
+var awesomeThoughts = "I am Dustin and I am AWESOME!";
+
+console.log(awesomeThoughts);
+
+var funThoughts = awesomeThoughts.replace("AWESOME!", "FUN");
+
+$("#main").append(funThoughts);
+
+var HTMLheaderName = "%data%";
+var name = "Dustin";
+var HTMLheaderRole = "%data%";
+var role = "Beginner Web Developer";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+
+$("#header").prepend(formattedName);

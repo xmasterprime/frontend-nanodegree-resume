7e467013a026477a621a227c2fe3e6e3cb910fc6
DESKTOP-Q6D10N1\dsison <dsison34@hotmail.com>
2017-07-08 15:52:16 -0700

    Lesson 3 Update

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,16 +6,24 @@ console.log(awesomeThoughts);
 
 var funThoughts = awesomeThoughts.replace("AWESOME!", "FUN");
 
-$("#main").append(funThoughts);
-
 var HTMLheaderName = "%data%";
 var name = "Dustin";
 var HTMLheaderRole = "%data%";
 var role = "Beginner Web Developer";
+var skills = ["Eating", "Sleeping"];
+
+var bio = {
+	"name" : "Dustin",
+	"role" : "QA Engineer",
+	"contact_info" : "dsison34@hotmail.com",
+	"picture_url" : "google.com",
+	"welcome_message" : "Hello.",
+	"skills" : "yeah" 
+};
 
 var formattedName = HTMLheaderName.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
 
 $("#header").prepend(formattedRole);
-
 $("#header").prepend(formattedName);
+$("#header").prepend(bio.name);
